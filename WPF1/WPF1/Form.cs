c6a2dc3f3e7e17c48dddfbe4d36e859dbdb85ea1
Cheyenne Hale <Cheyenne.C.Hale-1@ou.edu>
2019-02-06 11:41:14 -0600

    Finished.

---
+++ WPF1/WPF1/Form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class Form
+    {    
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public Form()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+
+        public Form(string name, string address, int zipCode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipCode;
+        }
+
+        override public string ToString()
+        {
+            return "Application: " + Name + " " + Address + " " + ZipCode;
+        }
+    }
+}
---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+     
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string zipAsString = txtZipCode.Text;
+
+            Form idk = new Form();
+            idk.ZipCode = Convert.ToInt32(zipAsString);
+            idk.Name = txtName.Text;
+            idk.Address = txtAddress.Text;
+
+            lstListBoxOutput.Items.Add(idk).ToString();
+            /*lstListBoxOutput.Items.Add(idk.Name);
+            lstListBoxOutput.Items.Add(idk.Address);*/
+            
+                
+
+        }
+
+        private void lstListBoxOutput_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+            
+        }
+    }
+    }
+
